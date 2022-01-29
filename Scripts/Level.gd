800a05a670a424f2beafb61c0ef90fa3763c917a
sleyral <s.leyral@gmail.com>
2022-01-29 12:31:53 +0100

    ajout son r1 sur le monde des morts

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -8,5 +8,7 @@ func _ready():
 func _on_EVENT_is_alive():
 	if(GAME.get_is_alive()):
 		$World_life_sound.pitch_scale = 1
+		$World_death_sound.pitch_scale = 0.01
 	if(!GAME.get_is_alive()):
 		$World_life_sound.pitch_scale = 0.01
+		$World_death_sound.pitch_scale = 1
