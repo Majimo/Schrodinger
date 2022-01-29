c367a8b135079ca98637b23af7604f3433ae7d21
sleyral <s.leyral@gmail.com>
2022-01-29 12:22:29 +0100

    Correction Event

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -1,11 +1,10 @@
 extends YSort
 
 func _ready():
+	EVENT.connect("is_alive", self, "_on_EVENT_is_alive")
 	_on_EVENT_is_alive()
 
-func _physics_process(delta):
-	EVENT.connect("is_alive", self, "_on_EVENT_is_alive")
-		
+
 func _on_EVENT_is_alive():
 	if(GAME.get_is_alive()):
 		$World_life_sound.pitch_scale = 1
