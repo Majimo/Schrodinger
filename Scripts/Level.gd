4017e2f949c54d88f0d6f43830d761d584cbfb41
Majimo <pierre.fervel@gmail.com>
2022-01-29 12:04:35 +0100

    Change Victory Scene & HTMLExport

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -1,12 +1,13 @@
 extends YSort
 
+func _ready():
+	_on_EVENT_is_alive()
+
 func _physics_process(delta):
 	EVENT.connect("is_alive", self, "_on_EVENT_is_alive")
 		
 func _on_EVENT_is_alive():
 	if(GAME.get_is_alive()):
-		print('vivant')
 		$World_life_sound.pitch_scale = 1
 	if(!GAME.get_is_alive()):
-		print('mort')
 		$World_life_sound.pitch_scale = 0.01
--- Scripts/Victory.gd
+++ Scripts/Victory.gd
@@ -1,20 +1,6 @@
-extends Node2D
+extends CanvasLayer
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(Input.is_action_just_pressed("ui_accept")):
-		print('changement de scene')
 		get_tree().change_scene("res://Scenes/Level.tscn")
